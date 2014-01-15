da2a9eca698eed346574914dbd25dcf1145cbd4f
maniax89 <andrew.p.turgeon@gmail.com>
2014-01-14 23:14:26 -0500

    added

---
+++ application/controllers/pages.php
@@ -0,0 +1,23 @@
+<?php
+
+class Pages extends CI_Controller {
+
+	public function view($page = 'home')
+	{
+		if ( ! file_exists('application/views/pages/'.$page.'.php'))
+		{
+			//no page
+			show_404();
+		}
+		
+		$data['title'] = ucfirst($page);
+		
+		$this->load->view('templates/header',$data);
+		$this->load->view('pages/'.$page, $data);
+		$this->load->view('templates/footer', $data);
+		
+	}
+
+}
+
+?>
