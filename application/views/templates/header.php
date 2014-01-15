3ac027e7247d06a38f387a4879bfdd5c27072e5e
maniax89 <andrew.p.turgeon@gmail.com>
2014-01-15 01:14:36 -0500

    changed so the nav appears on every page, mobile width defined

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,6 +3,7 @@
         <h1>AndrewPTurgeon</h1>
     <head>
         <title><?php echo $title ?> - Andrew Turgeon</title>
+		<meta name="viewport" content="width=device-width, user-scalable=no">
         <link rel="stylesheet" href="<?php echo SCRIPT_ROOT; ?>/css/main.css" />
     </head>
     
