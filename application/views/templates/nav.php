5ce52fdfda4bfc91ab8c41599a31f03e00d177f2
maniax89 <andrew.p.turgeon@gmail.com>
2014-01-15 00:31:01 -0500

    fixed htaccess file to rewrite URLs correctly

--- application/views/templates/nav.php
+++ application/views/templates/nav.php
@@ -1,5 +1,5 @@
 <ul class="main_nav">
     <li><a href="<?php echo SCRIPT_ROOT; ?>">Home</a></li>
-    <li><a href="<?php echo SCRIPT_ROOT; ?>/index.php/about">About</a></li>
-    <li><a href="<?php echo SCRIPT_ROOT; ?>/index.php/news">News</a></li>
+    <li><a href="<?php echo SCRIPT_ROOT; ?>/about">About</a></li>
+    <li><a href="<?php echo SCRIPT_ROOT; ?>/news">News</a></li>
 </ul>
